99122650af80beabc67bb674ddb9c36d6840d8ce
Brendan Emery <brendan.be.emery@bmw.de>
2025-05-12 14:01:18 +0200

    lola: Add NodeIdentifierCopier and unit tests
    Issue: SWP-181102
    GIT_ORIGIN_SPP_REV_ID: 82f6818a0299c4d2d9c4a6ad746e5177828d297a

---
+++ score/mw/com/impl/bindings/lola/messaging/node_identifier_copier_test.cpp
@@ -0,0 +1,314 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#include "score/mw/com/impl/bindings/lola/messaging/node_identifier_copier.h"
+
+#include "score/mw/com/impl/bindings/lola/element_fq_id.h"
+
+#include <score/assert.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <sys/types.h>
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <set>
+#include <shared_mutex>
+#include <unordered_map>
+#include <vector>
+
+namespace score::mw::com::impl::lola
+{
+
+const ElementFqId kDummyElementFqId{2U, 3U, 4U, ElementType::EVENT};
+const ElementFqId kElementFqIdNotInMap{222U, 333U, 444U, ElementType::EVENT};
+const pid_t kPidNotInMap{10000U};
+constexpr auto kMaxBufferSize = std::tuple_size<NodeIdTmpBufferType>::value;
+
+class CopyNodeIdentiersFixture : public ::testing::Test
+{
+  public:
+    CopyNodeIdentiersFixture& WithANodeIdMapContainingPids(const ElementFqId element_fq_id_map_key,
+                                                           std::vector<pid_t> pids)
+    {
+        std::set<pid_t> pid_set{pids.begin(), pids.end()};
+        node_id_map_.insert({element_fq_id_map_key, std::move(pid_set)});
+        return *this;
+    }
+
+    std::uint32_t GetNumberOfPidsInBuffer() const
+    {
+        std::uint32_t number_of_valid_node_ids{0U};
+        for (const auto node_id : node_id_tmp_buffer_)
+        {
+            if (node_id != 0U)
+            {
+                number_of_valid_node_ids += 1U;
+            }
+        }
+        return number_of_valid_node_ids;
+    }
+
+    bool ArePidsInNodeBuffer(std::vector<pid_t> expected_pids) const
+    {
+        EXPECT_EQ(expected_pids.size(), GetNumberOfPidsInBuffer());
+        if (expected_pids.size() != GetNumberOfPidsInBuffer())
+        {
+            return false;
+        }
+
+        for (std::size_t i = 0; i < expected_pids.size(); ++i)
+        {
+            const auto actual_pid = node_id_tmp_buffer_[i];
+            const auto expected_pid = expected_pids[i];
+            EXPECT_EQ(actual_pid, expected_pid);
+            if (actual_pid != expected_pid)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    using MapType = std::unordered_map<ElementFqId, std::set<pid_t>>;
+    MapType node_id_map_{};
+    NodeIdTmpBufferType node_id_tmp_buffer_{};
+    std::shared_mutex map_mutex_{};
+};
+
+using CopyNodeIdentiersEmptyMapFixture = CopyNodeIdentiersFixture;
+TEST_F(CopyNodeIdentiersEmptyMapFixture, CopyingNodesReturnsNoFutherIdsAvailable)
+{
+    // Given a map containg no node ids
+    const std::vector<pid_t> node_ids{};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying node identifiers
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, kPidNotInMap);
+
+    // Then no further ids are available
+    EXPECT_FALSE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersEmptyMapFixture, CopyingNodesCopiesNoIds)
+{
+    // Given a map containg no node ids
+    const std::vector<pid_t> node_ids{};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying node identifiers
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, kPidNotInMap);
+
+    // Then no node ids are copied to the buffer
+    EXPECT_EQ(num_of_nodeids_copied, 0U);
+    EXPECT_EQ(GetNumberOfPidsInBuffer(), 0U);
+}
+
+using CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture = CopyNodeIdentiersFixture;
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture, CopyingNodesReturnsNoFurtherIdsAvailable)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then no further ids are available
+    EXPECT_FALSE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture, CopyingNodesCopiesAllIdsFromMap)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then all node ids are copied to the buffer
+    const std::vector<pid_t> expected_copied_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    EXPECT_EQ(num_of_nodeids_copied, expected_copied_ids.size());
+    EXPECT_TRUE(ArePidsInNodeBuffer(expected_copied_ids));
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture,
+       CopyingNodesStartingFromSecondIdReturnsNoFurtherIdsAvailable)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map starting from the second pid in the map
+    const auto second_pid = node_ids[1];
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, second_pid);
+
+    // Then no further ids are available
+    EXPECT_FALSE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture,
+       CopyingNodesStartingFromSecondIdCopiesAllIdsExceptFirst)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map starting from the second pid in the map
+    const auto second_pid = node_ids[1];
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, second_pid);
+
+    // Then all node ids starting from the second id are copied to the buffer
+    const std::vector<pid_t> expected_copied_ids{2, 3, 4, 5, 6, 7, 8, 9, 10};
+    EXPECT_EQ(num_of_nodeids_copied, expected_copied_ids.size());
+    EXPECT_TRUE(ArePidsInNodeBuffer(expected_copied_ids));
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture,
+       CopyingNodesStartingFromIdNotInMapReturnsNoFurtherIdsAvailable)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map starting from a pid that is not in the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, kPidNotInMap);
+
+    // Then no further ids are available
+    EXPECT_FALSE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture, CopyingNodesStartingFromIdNotInMapCopiesNoIds)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map starting from a pid that is not in the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, kPidNotInMap);
+
+    // Then no node ids are copied to the buffer
+    EXPECT_EQ(num_of_nodeids_copied, 0U);
+    EXPECT_EQ(GetNumberOfPidsInBuffer(), 0U);
+}
+
+TEST_F(CopyNodeIdentiersMapWithLessNodesThanMaxBufferSizeFixture, CopyingNodesForElementFqIdNotInMapCopiesNoIds)
+{
+    // Given a map containing less node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers for an ElementFqId that's not in the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kElementFqIdNotInMap, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then no node ids are copied to the buffer
+    EXPECT_EQ(num_of_nodeids_copied, 0U);
+    EXPECT_EQ(GetNumberOfPidsInBuffer(), 0U);
+}
+
+using CopyNodeIdentiersMapWithNodesEqualToMaxBufferSizeFixture = CopyNodeIdentiersFixture;
+TEST_F(CopyNodeIdentiersMapWithNodesEqualToMaxBufferSizeFixture, CopyingNodesReturnsNoFurtherIdsAvailable)
+{
+    // Given a map containing the same number of node ids as the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    ASSERT_EQ(node_ids.size(), kMaxBufferSize);
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then no further ids are available
+    EXPECT_FALSE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersMapWithNodesEqualToMaxBufferSizeFixture, CopyingNodesCopiesAllIdsFromMap)
+{
+    // Given a map containing the same number of node ids as the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    ASSERT_EQ(node_ids.size(), kMaxBufferSize);
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then all the ids are copied to the buffer
+    const std::vector<pid_t> expected_copied_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    EXPECT_EQ(num_of_nodeids_copied, kMaxBufferSize);
+    EXPECT_TRUE(ArePidsInNodeBuffer(expected_copied_ids));
+}
+
+using CopyNodeIdentiersMapWithMoreNodesThanMaxBufferSizeFixture = CopyNodeIdentiersFixture;
+TEST_F(CopyNodeIdentiersMapWithMoreNodesThanMaxBufferSizeFixture, CopyingNodesReturnsFurtherIdsAvailable)
+{
+    // Given a map containing more node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+    ASSERT_GT(node_ids.size(), kMaxBufferSize);
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then further ids are available
+    EXPECT_TRUE(further_ids_available);
+}
+
+TEST_F(CopyNodeIdentiersMapWithMoreNodesThanMaxBufferSizeFixture, CopyingNodesCopiesFirstMaxBufferSizeIdsFromMap)
+{
+    // Given a map containing more node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+    ASSERT_GT(node_ids.size(), kMaxBufferSize);
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, node_ids[0]);
+
+    // Then the first <max buffer size> ids are copied to the buffer
+    const std::vector<pid_t> expected_copied_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    EXPECT_EQ(num_of_nodeids_copied, kMaxBufferSize);
+    EXPECT_TRUE(ArePidsInNodeBuffer(expected_copied_ids));
+}
+
+TEST_F(CopyNodeIdentiersMapWithMoreNodesThanMaxBufferSizeFixture,
+       CopyingNodesStartingFromSecondIdCopiesMaxBufferSizeIdsStartingFromSecond)
+{
+    // Given a map containing more node ids than the max buffer size
+    const std::vector<pid_t> node_ids{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+    ASSERT_GT(node_ids.size(), kMaxBufferSize);
+    WithANodeIdMapContainingPids(kDummyElementFqId, node_ids);
+
+    // When copying the node identifiers from the map
+    const auto second_pid = node_ids[1];
+    const auto [num_of_nodeids_copied, further_ids_available] =
+        CopyNodeIdentifiers<MapType>(kDummyElementFqId, node_id_map_, map_mutex_, node_id_tmp_buffer_, second_pid);
+
+    // Then the first <max buffer size> ids starting from the second pid are copied to the buffer
+    const std::vector<pid_t> expected_copied_ids{2,  3,  4,  5,  6,  7,  8,  9,  10, 11,
+                                                 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+    EXPECT_EQ(num_of_nodeids_copied, kMaxBufferSize);
+    EXPECT_TRUE(ArePidsInNodeBuffer(expected_copied_ids));
+}
+
+}  // namespace score::mw::com::impl::lola
--- score/mw/com/impl/bindings/lola/messaging/notify_event_handler.cpp
+++ score/mw/com/impl/bindings/lola/messaging/notify_event_handler.cpp
@@ -12,11 +12,12 @@
  ********************************************************************************/
 #include "score/mw/com/impl/bindings/lola/messaging/notify_event_handler.h"
 
-#include "score/os/errno_logging.h"
 #include "score/mw/com/impl/bindings/lola/element_fq_id.h"
 #include "score/mw/com/impl/bindings/lola/messaging/messages/message_element_fq_id.h"
 #include "score/mw/com/impl/bindings/lola/messaging/messages/message_outdated_nodeid.h"
+#include "score/mw/com/impl/bindings/lola/messaging/node_identifier_copier.h"
 
+#include "score/os/errno_logging.h"
 #include "score/mw/log/logging.h"
 
 #include <score/assert.hpp>
@@ -32,73 +33,6 @@ namespace
 {
 
 constexpr std::uint8_t kMaxReceiveHandlersPerEvent{5U};
-
-using NodeIdTmpBufferType = std::array<pid_t, 20>;
-
-/// \brief Copies node identifiers (pid) contained within (container) values of a map into a given buffer under
-///        read-lock
-/// \details
-/// \tparam MapType Type of the Map. It is implicitly expected, that the key of MapType is of type ElementFqId and
-///         the mapped_type is a std::set (or at least some forward iterable container type, which supports
-///         lower_bound()), which contains values, which directly or indirectly contain a node identifier (pid_t)
-///
-/// \param event_id fully qualified event id for lookup in _src_map_
-/// \param src_map map, where key_type = ElementFqId and mapped_type is some forward iterable container
-/// \param src_map_mutex mutex to be used to lock the map during copying.
-/// \param dest_buffer buffer, where to copy the node identifiers
-/// \param start start identifier (pid_t) where to start search with.
-///
-/// \return pair containing number of node identifiers, which have been copied and a bool, whether further ids could
-///         have been copied, if buffer would have been larger.
-template <typename MapType>
-// Suppress "AUTOSAR C++14 A15-5-3" rule findings. This rule states: "The std::terminate() function shall not be
-// called implicitly". std::terminate() is implicitly called from 'dest_buffer.at()' in case the index goes outside
-// the array's range, as we starts from index zero and we check that the buffer size is greater than zero and later
-// we compare the index with the buffer's size, so no way for the index to go outside array's range and throw
-// std::out_of_range which leds to std::terminate().
-// coverity[autosar_cpp14_a15_5_3_violation : FALSE]
-std::pair<std::uint8_t, bool> CopyNodeIdentifiers(ElementFqId event_id,
-                                                  MapType& src_map,
-                                                  std::shared_mutex& src_map_mutex,
-                                                  NodeIdTmpBufferType& dest_buffer,
-                                                  pid_t start) noexcept
-{
-    std::uint8_t num_nodeids_copied{0U};
-    bool further_ids_avail{false};
-    // Suppress "AUTOSAR C++14 M0-1-3" rule findings. This rule states: "There shall be no dead code".
-    //  This is a RAII Pattern, which binds the life cycle of a resource that must be acquired before use.
-    // coverity[autosar_cpp14_m0_1_3_violation]
-    std::shared_lock<std::shared_mutex> read_lock(src_map_mutex);
-    if (src_map.empty() == false)
-    {
-        auto search = src_map.find(event_id);
-        if (search != src_map.end())
-        {
-            // we copy the target_node_identifiers to a tmp-array under lock ...
-            for (auto iter = search->second.lower_bound(start); iter != search->second.cend(); iter++)
-            {
-                SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(dest_buffer.size() > 0U,
-                                       "HandlerBase::CopyNodeIdentifiers destination buffer has size zero!");
-                // Suppress "AUTOSAR C++14 M5-0-3" rule findings. This rule states: "A cvalue expression shall
-                // not be implicitly converted to a different underlying type"
-                // That return `pid_t` type as a result. Tolerated implicit conversion as underlying types are the
-                // same coverity
-                // coverity[autosar_cpp14_m5_0_3_violation]
-                dest_buffer.at(num_nodeids_copied) = *iter;
-                num_nodeids_copied++;
-                if (num_nodeids_copied == dest_buffer.size())
-                {
-                    if (std::distance(iter, search->second.cend()) > 1)
-                    {
-                        further_ids_avail = true;
-                    }
-                    break;
-                }
-            }
-        }
-    }
-    return {num_nodeids_copied, further_ids_avail};
-}
 
 }  // namespace
 
