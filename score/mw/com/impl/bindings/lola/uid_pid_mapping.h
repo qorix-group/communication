a4930af915b9ec03130d904187a5851276116ce6
Brendan Emery <brendan.be.emery@bmw.de>
2025-05-12 16:00:18 +0200

    lola: Extract UidPidMappingEntry into separate file
    Issue: SWP-181102
    GIT_ORIGIN_SPP_REV_ID: 03292d37e71060bff162d46e49ec97c97257313a

--- score/mw/com/impl/bindings/lola/uid_pid_mapping.h
+++ score/mw/com/impl/bindings/lola/uid_pid_mapping.h
@@ -13,49 +13,18 @@
 #ifndef SCORE_MW_COM_IMPL_BINDINGS_LOLA_UID_PID_MAPPING_H
 #define SCORE_MW_COM_IMPL_BINDINGS_LOLA_UID_PID_MAPPING_H
 
+#include "score/mw/com/impl/bindings/lola/uid_pid_mapping_entry.h"
+
 #include "score/containers/dynamic_array.h"
 #include "score/memory/shared/atomic_indirector.h"
 
 #include <sys/types.h>
-#include <atomic>
 #include <cstdint>
 #include <memory>
 #include <optional>
-#include <utility>
 
 namespace score::mw::com::impl::lola
 {
-
-class UidPidMappingEntry
-{
-  public:
-    enum class MappingEntryStatus : std::uint16_t
-    {
-        kUnused = 0u,
-        kUsed,
-        kUpdating,
-        kInvalid,  // this is a value, which we shall NOT see in an entry!
-    };
-
-    // \brief our key-type is a combination of 4 byte status and 4 byte uid
-    using key_type = std::uint64_t;
-    // \brief we use key_type for our lock-free sync algo -> atomic access needs to be always lock-free therefore
-    static_assert(std::atomic<key_type>::is_always_lock_free);
-    // \brief we are encoding the uid into our key-type and have foreseen 4 byte for it!
-    static_assert(sizeof(uid_t) <= 4);
-
-    /// \brief Load key atomically and return its parts as a pair.
-    /// \return parts, which make up the key
-    std::pair<MappingEntryStatus, uid_t> GetStatusAndUidAtomic() noexcept;
-    void SetStatusAndUidAtomic(MappingEntryStatus status, uid_t uid) noexcept;
-    static key_type CreateKey(MappingEntryStatus status, uid_t uid) noexcept;
-    // Suppress "AUTOSAR C++14 M11-0-1" rule findings. This rule states: "Member data in non-POD class types shall
-    // be private.". There are no class invariants to maintain so member variable can be safely accessed directly.
-    // coverity[autosar_cpp14_m11_0_1_violation]
-    std::atomic<key_type> key_uid_status_{};
-    // coverity[autosar_cpp14_m11_0_1_violation]
-    pid_t pid_{};
-};
 
 namespace detail
 {
