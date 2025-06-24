29d0ab32730d8f1a1bf51f0cbe000743264f83a8
Brendan Emery <brendan.be.emery@bmw.de>
2025-06-24 10:41:34 +0200

    Revert "Revert "mw/com: Add equality operator to ServiceTypeDeployment classes""
    This reverts commit 84b9b452e3aa921e95835b3cd9bb65ab355c31eb.
    
    Issue: SWP-167609
    GIT_ORIGIN_SPP_REV_ID: 1f5e9e0c65bb940cb679f1580af7a462285e8d29

--- score/mw/com/impl/configuration/service_type_deployment.cpp
+++ score/mw/com/impl/configuration/service_type_deployment.cpp
@@ -128,4 +128,9 @@ std::string_view ServiceTypeDeployment::ToHashString() const noexcept
     return hash_string_;
 }
 
+bool operator==(const ServiceTypeDeployment& lhs, const ServiceTypeDeployment& rhs) noexcept
+{
+    return lhs.binding_info_ == rhs.binding_info_;
+}
+
 }  // namespace score::mw::com::impl
--- score/mw/com/impl/configuration/service_type_deployment_test.cpp
+++ score/mw/com/impl/configuration/service_type_deployment_test.cpp
@@ -12,6 +12,7 @@
  ********************************************************************************/
 #include "score/mw/com/impl/configuration/service_type_deployment.h"
 
+#include "lola_service_id.h"
 #include "score/mw/com/impl/configuration/test/configuration_test_resources.h"
 
 #include <gmock/gmock.h>
@@ -28,6 +29,7 @@ using ::testing::Pair;
 using ::testing::UnorderedElementsAre;
 
 ServiceIdentifierType dummy_service = make_ServiceIdentifierType("foo", 1U, 0U);
+const LolaServiceId kLolaServiceId{123U};
 
 TEST(ServiceTypeDeploymentTest, CanConstructFromLolaServiceTypeDeployment)
 {
@@ -55,6 +57,28 @@ TEST_F(ServiceTypeDeploymentFixture, CanCreateFromSerializedLolaObject)
     ServiceTypeDeployment reconstructed_unit{serialized_unit};
 
     ExpectServiceTypeDeploymentObjectsEqual(reconstructed_unit, unit);
+}
+
+TEST_F(ServiceTypeDeploymentFixture, ComparingSameDeploymentsReturnsTrue)
+{
+    // When comparing two ServiceTypeDeployments containing the same data
+    const ServiceTypeDeployment unit{MakeLolaServiceTypeDeployment(kLolaServiceId)};
+    const ServiceTypeDeployment unit2{MakeLolaServiceTypeDeployment(kLolaServiceId)};
+    const auto are_equal = unit == unit2;
+
+    // Then the result is true
+    EXPECT_TRUE(are_equal);
+}
+
+TEST_F(ServiceTypeDeploymentFixture, ComparingDifferentDeploymentsReturnsFalse)
+{
+    // When comparing two ServiceTypeDeployments containing different data
+    const ServiceTypeDeployment unit{MakeLolaServiceTypeDeployment(kLolaServiceId)};
+    const ServiceTypeDeployment unit2{MakeLolaServiceTypeDeployment(kLolaServiceId + 1U)};
+    const auto are_equal = unit == unit2;
+
+    // Then the result is false
+    EXPECT_FALSE(are_equal);
 }
 
 TEST(ServiceTypeDeploymentTest, CanCreateFromSerializedBlankObject)
