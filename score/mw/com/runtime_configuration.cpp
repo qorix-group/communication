97bc1305ecd15eadf2409d06245b181eaf5b4bba
Xavier Bonaventura <xavier.bonaventura@bmw.de>
2025-07-30 10:51:31 +0200

    aas: Adapt code to v2 version of amp::span
    amp::span v1 was having a signed size type, this has been changed to
    be aligned with the C++ standard.
    The code needs to be adapted for these changs.
    
    Issue: ORIONINIT-102294
    GIT_ORIGIN_SPP_REV_ID: 92c14f04b4f9765668c9175cab42fcdf50dd687d

--- score/mw/com/runtime_configuration.cpp
+++ score/mw/com/runtime_configuration.cpp
@@ -46,7 +46,8 @@ RuntimeConfiguration::RuntimeConfiguration(filesystem::Path configuration_path)
 // NOLINTNEXTLINE(modernize-avoid-c-arrays):C-style array tolerated for command line arguments
 RuntimeConfiguration::RuntimeConfiguration(const std::int32_t argc, score::StringLiteral argv[]) : configuration_path_{}
 {
-    const score::cpp::span<const score::StringLiteral> command_line_arguments(argv, argc);
+    const score::cpp::span<const score::StringLiteral> command_line_arguments(
+        argv, static_cast<score::cpp::span<const score::StringLiteral>::size_type>(argc));
     auto configuration_path = ParseConfigurationPath(command_line_arguments);
     configuration_path_ =
         configuration_path.has_value() ? std::move(configuration_path).value() : kDefaultConfigurationPath;
@@ -60,8 +61,7 @@ const filesystem::Path& RuntimeConfiguration::GetConfigurationPath() const
 std::optional<filesystem::Path> RuntimeConfiguration::ParseConfigurationPath(
     const score::cpp::span<const score::StringLiteral> command_line_args)
 {
-    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD(command_line_args.size() >= 0);
-    const auto num_args = static_cast<std::uint32_t>(command_line_args.size());
+    const auto num_args = command_line_args.size();
 
     std::optional<std::string> configuration_path{};
     for (std::uint32_t arg_idx = 0; arg_idx < num_args; arg_idx++)
