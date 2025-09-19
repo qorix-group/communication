70cd1facf06395f69923a00dc081cd06bd6f997b
Markus Hosch <markus.hosch@bmw.de>
2025-09-19 09:10:16 +0200

    Introduce max_align_t for QNX
    ipc_eample_rs didn't compile for QNX since there was no definition of
    max_align_t. Add one locally and use it.
    
    GIT_ORIGIN_SPP_REV_ID: 6317783d4a6a81edc49459e5c1c923763dea6824

--- score/mw/com/impl/plumbing/sample_ptr.rs
+++ score/mw/com/impl/plumbing/sample_ptr.rs
@@ -12,8 +12,29 @@
  ********************************************************************************/
 use std::mem::ManuallyDrop;
 
+#[cfg(target_os = "nto")]
+mod util_nto {
+    #![allow(non_camel_case_types)]
+
+    // There is no canonical definition of c_longdouble in Rust. For both AArch64 and x86_64,
+    // however, the size and alignment properties are that of the gcc __int128 which corresponds (at
+    // least on rustc 1.78+ with LLVM 18, see
+    // https://blog.rust-lang.org/2024/03/30/i128-layout-update/) to u128. Use this instead until we
+    // get native f128 support.
+    #[derive(Copy, Clone, Default, Debug)]
+    pub struct max_align_t {
+        _ll: libc::c_longlong,
+        _ld: u128,
+    }
+}
+
+#[cfg(target_os = "nto")]
+use util_nto::max_align_t;
+#[cfg(not(target_os = "nto"))]
+use libc::max_align_t;
+
 // @todo check whether we can get this info "somehow" from C++ code
-type CustomDeleterAlignment = libc::max_align_t;
+type CustomDeleterAlignment = max_align_t;
 const CUSTOM_DELETER_SIZE: usize = 32;
 
 #[repr(C)]
