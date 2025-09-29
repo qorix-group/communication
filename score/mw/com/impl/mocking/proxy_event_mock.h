594fa363291352ccae3ef3a5a8174a63240e3a93
Brendan Emery <brendan.be.emery@bmw.de>
2025-09-29 13:17:27 +0200

    mw/com: Add binding independent mock classes
    GIT_ORIGIN_SPP_REV_ID: c316ceac58779a62815865033b6530ae2b7c7bf4

---
+++ score/mw/com/impl/mocking/proxy_event_mock.h
@@ -0,0 +1,70 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_MW_COM_IMPL_MOCKING_PROXY_EVENT_MOCK_H
+#define SCORE_MW_COM_IMPL_MOCKING_PROXY_EVENT_MOCK_H
+
+#include "score/mw/com/impl/event_receive_handler.h"
+#include "score/mw/com/impl/plumbing/sample_ptr.h"
+#include "score/mw/com/impl/subscription_state.h"
+
+#include "score/result/result.h"
+
+#include <score/callback.hpp>
+
+#include <cstdint>
+
+namespace score::mw::com::impl
+{
+
+class ProxyEventBaseMock
+{
+  public:
+    ProxyEventBaseMock() = default;
+    virtual ~ProxyEventBaseMock() = default;
+
+    virtual ResultBlank Subscribe(const std::size_t) = 0;
+    virtual void Unsubscribe() = 0;
+    virtual SubscriptionState GetSubscriptionState() const = 0;
+    virtual std::size_t GetFreeSampleCount() const = 0;
+    virtual Result<std::size_t> GetNumNewSamplesAvailable() = 0;
+    virtual ResultBlank SetReceiveHandler(EventReceiveHandler) = 0;
+    virtual ResultBlank UnsetReceiveHandler() = 0;
+
+  protected:
+    ProxyEventBaseMock(const ProxyEventBaseMock&) = default;
+    ProxyEventBaseMock(ProxyEventBaseMock&&) noexcept = default;
+    ProxyEventBaseMock& operator=(ProxyEventBaseMock&&) & noexcept = default;
+    ProxyEventBaseMock& operator=(const ProxyEventBaseMock&) & = default;
+};
+
+template <typename SampleType>
+class ProxyEventMock : public ProxyEventBaseMock
+{
+  public:
+    ProxyEventMock() = default;
+    ~ProxyEventMock() override = default;
+
+    using Callback = score::cpp::callback<void(SamplePtr<SampleType>), 80U>;
+
+    virtual Result<std::size_t> GetNewSamples(Callback&&, const std::size_t) = 0;
+
+  protected:
+    ProxyEventMock(const ProxyEventMock&) = default;
+    ProxyEventMock(ProxyEventMock&&) noexcept = default;
+    ProxyEventMock& operator=(ProxyEventMock&&) & noexcept = default;
+    ProxyEventMock& operator=(const ProxyEventMock&) & = default;
+};
+
+}  // namespace score::mw::com::impl
+
+#endif  // SCORE_MW_COM_IMPL_MOCKING_PROXY_EVENT_MOCK_H
---
+++ score/mw/com/impl/mocking/skeleton_mock_impl.h
@@ -0,0 +1,32 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_IMPL_H
+#define SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_IMPL_H
+
+#include "score/mw/com/impl/mocking/skeleton_mock.h"
+
+#include <gmock/gmock.h>
+
+namespace score::mw::com::impl
+{
+
+class SkeletonMockImpl : public SkeletonMock
+{
+  public:
+    MOCK_METHOD(ResultBlank, OfferService, (), (override));
+    MOCK_METHOD(void, StopOfferService, (), (override));
+};
+
+}  // namespace score::mw::com::impl
+
+#endif  // SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_IMPL_H
