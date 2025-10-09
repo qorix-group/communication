f18995bace64894e0ef96ec600b5823bfc1a7923
Brendan Emery <brendan.be.emery@bmw.de>
2025-10-09 13:04:29 +0200

    mw/com: Add configuration test for applicationID
    GIT_ORIGIN_SPP_REV_ID: 870dbb69e6d29499267d79948fa9646bab8aabca

--- score/mw/com/impl/configuration/config_parser_test.cpp
+++ score/mw/com/impl/configuration/config_parser_test.cpp
@@ -126,6 +126,7 @@ TEST_F(ConfigParserFixture, ParseExampleJson)
     EXPECT_EQ(config.GetGlobalConfiguration().GetReceiverMessageQueueSize(QualityType::kASIL_QM), 8);
     EXPECT_EQ(config.GetGlobalConfiguration().GetReceiverMessageQueueSize(QualityType::kASIL_B), 5);
     EXPECT_EQ(config.GetGlobalConfiguration().GetSenderMessageQueueSize(), 12);
+    EXPECT_EQ(config.GetGlobalConfiguration().GetApplicationId(), 1234);
 
     EXPECT_EQ(config.GetGlobalConfiguration().GetShmSizeCalcMode(), ShmSizeCalculationMode::kSimulation);
 }
