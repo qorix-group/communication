46312db58e5d4784af390176b9118df960b76b11
Manuel Fehlhammer <manuel.fehlhammer@bmw.de>
2025-10-18 14:46:56 +0200

    mw/com/impl/binding/mock_binding: Add ProxyMethod mock
    Added mock class for ProxyMethodBinding.
    
    GIT_ORIGIN_SPP_REV_ID: c1b38e30a2a7531827f29a0e75e0452660fe81a7

---
+++ score/mw/com/impl/bindings/mock_binding/proxy_method.h
@@ -0,0 +1,43 @@
+/********************************************************************************
+* Copyright (c) 2025 Contributors to the Eclipse Foundation
+*
+* See the NOTICE file(s) distributed with this work for additional
+* information regarding copyright ownership.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Apache License Version 2.0 which is available at
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* SPDX-License-Identifier: Apache-2.0
+********************************************************************************/
+#ifndef SCORE_MW_COM_IMPL_BINDINGS_MOCK_BINDING_PROXY_METHOD_H
+#define SCORE_MW_COM_IMPL_BINDINGS_MOCK_BINDING_PROXY_METHOD_H
+
+#include "score/mw/com/impl/proxy_method_binding.h"
+
+#include <score/assert.hpp>
+
+#include <gmock/gmock.h>
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <utility>
+#include <vector>
+
+namespace score::mw::com::impl::mock_binding
+{
+
+class ProxyMethod : public ProxyMethodBinding
+{
+ public:
+   ~ProxyMethod() override = default;
+
+   MOCK_METHOD(score::Result<void*>, AllocateInArgs, (std::size_t), (override));
+   MOCK_METHOD(score::Result<void*>, AllocateReturnType, (std::size_t), (override));
+   MOCK_METHOD(ResultBlank, DoCall, (std::size_t, score::cpp::stop_token), (override));
+};
+
+
+}  // namespace score::mw::com::impl::mock_binding
+
+#endif  // SCORE_MW_COM_IMPL_BINDINGS_MOCK_BINDING_PROXY_METHOD_H
