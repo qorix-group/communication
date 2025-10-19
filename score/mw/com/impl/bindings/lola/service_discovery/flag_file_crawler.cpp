4a5c94f92bb9412191e3d613643b6830c1a2f24e
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2025-10-19 17:06:55 +0200

    lola: add QNX8 workaround for poll
    in QNX8, event based poll does not work hence as a temp workaround a small timeout is added to poll that wakes up and checks for any events in the queue.This has some performance impacts due to the nature of periodic wakeups.
    
    GIT_ORIGIN_SPP_REV_ID: d66494f9b7c79e91d112a4c1eee5d9dad9a85c4e

--- score/mw/com/impl/bindings/lola/service_discovery/flag_file_crawler.cpp
+++ score/mw/com/impl/bindings/lola/service_discovery/flag_file_crawler.cpp
@@ -334,8 +334,9 @@ auto FlagFileCrawler::AddWatchToInotifyInstance(const EnrichedInstanceIdentifier
     }
 
     const auto search_path = std::move(directory_creation_result).value();
-    const auto watch_descriptor = inotify_instance_.AddWatch(
-        search_path.Native(), os::Inotify::EventMask::kInCreate | os::Inotify::EventMask::kInDelete);
+    const auto event_mask = os::Inotify::EventMask::kInCreate | os::Inotify::EventMask::kInDelete;
+
+    const auto watch_descriptor = inotify_instance_.AddWatch(search_path.Native(), event_mask);
     if (!(watch_descriptor.has_value()))
     {
         mw::log::LogError("lola") << "Could not add watch for" << search_path << ":"
@@ -363,7 +364,9 @@ auto FlagFileCrawler::AddWatchToInotifyInstance(const EnrichedInstanceIdentifier
         }
         return score::MakeUnexpected(ComErrc::kBindingFailure, "Could not add watch for service id");
     }
-
+    mw::log::LogDebug("lola") << "Successfully added inotify watch: path=" << search_path.Native()
+                              << ", descriptor=" << watch_descriptor.value().GetUnderlying()
+                              << ", mask=" << static_cast<int>(event_mask);
     return watch_descriptor.value();
 }
 
