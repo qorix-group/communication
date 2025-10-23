3fcffe5f52d00d20382b3fae2fafeac6e84b2a8f
Brendan Emery <brendan.be.emery@bmw.de>
2025-10-23 09:00:32 +0200

    Revert "Revert "mw/com: Add binding independent mock classes""
    This reverts commit 6b8b3992bcb4d2f5aff3922e57dd421b85aa5598.
    
    GIT_ORIGIN_SPP_REV_ID: 406ac258d7b514c3d817c3c0dc4b2e522263cfa8

---
+++ score/mw/com/impl/mocking/skeleton_mock.h
@@ -0,0 +1,41 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_H
+#define SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_H
+
+#include "score/result/result.h"
+
+#include <cstdint>
+
+namespace score::mw::com::impl
+{
+
+class SkeletonMock
+{
+  public:
+    SkeletonMock() = default;
+    virtual ~SkeletonMock() = default;
+
+    virtual ResultBlank OfferService() = 0;
+    virtual void StopOfferService() = 0;
+
+  protected:
+    SkeletonMock(const SkeletonMock&) = default;
+    SkeletonMock(SkeletonMock&&) noexcept = default;
+    SkeletonMock& operator=(SkeletonMock&&) & noexcept = default;
+    SkeletonMock& operator=(const SkeletonMock&) & = default;
+};
+
+}  // namespace score::mw::com::impl
+
+#endif  // SCORE_MW_COM_IMPL_MOCKING_SKELETON_MOCK_H
