6e0f05b35ccb0e34e82c2f608b82e16c164f6020
Brendan Emery <brendan.be.emery@bmw.de>
2025-10-27 08:44:13 +0100

    mw/com: Add static asserts about ElementId types
    Currently, Event, Field and Method Ids are all the same type. Therefore,
    ElementId within ElementFqId can be an alias to any one of them.
    However, if one of these types change, then ElementId must be a
    variant<EventId, FieldId, MethodId>. We add static asserts to check
    this.
    
    GIT_ORIGIN_SPP_REV_ID: 44c33f10bbb76a920e8045329c4e55e8402e6ceb

--- score/mw/com/impl/bindings/lola/element_fq_id.h
+++ score/mw/com/impl/bindings/lola/element_fq_id.h
@@ -14,6 +14,8 @@
 #define SCORE_MW_COM_IMPL_BINDINGS_LOLA_ELEMENT_FQ_ID_H
 
 #include "score/mw/com/impl/configuration/lola_event_id.h"
+#include "score/mw/com/impl/configuration/lola_field_id.h"
+#include "score/mw/com/impl/configuration/lola_method_id.h"
 #include "score/mw/com/impl/configuration/lola_service_id.h"
 #include "score/mw/com/impl/configuration/lola_service_instance_id.h"
 #include "score/mw/com/impl/service_element_type.h"
@@ -49,6 +51,11 @@ class ElementFqId
     using ServiceId = LolaServiceId;
     using ElementId = LolaEventId;
     using InstanceId = LolaServiceInstanceId::InstanceId;
+
+    static_assert(std::is_same_v<LolaEventId, LolaFieldId>,
+                  "LolaEventId must be the same as LolaFieldId, otherwise ElementId must be a variant!");
+    static_assert(std::is_same_v<LolaEventId, LolaMethodId>,
+                  "LolaEventId must be the same as LolaMethodId, otherwise ElementId must be a variant!");
 
     /// \brief default ctor initializing all members to their related max value.
     /// \details constructs an "invalid" ElementFqId.
