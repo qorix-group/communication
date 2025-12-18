3792dbdce9e137e11e5a893f52509abfe732ba2a
Stephan Muench <stephan.sm.muench@bmw.de>
2025-12-18 11:29:05 +0100

    lib/message_passing: mitigate (iterator-based) pointer arithmetic
    According to MISRA Rule M5-0-15, the only allowed form of pointer
    arithmetic
    shall be array indexing or incrementing/decrementing pointer-based
    iterators.
    
    In order to not having to suppress both coverity as well as clang-tidy
    findings
    (from llvm version 21 onwards), we replace the pointer arithmetic by
    making use
    of `std::next()` which is also a more readable way of acheiving the
    same.
    
    GIT_ORIGIN_SPP_REV_ID: da02b5aadf8b759b212ef952635252daab35b3fc

--- score/message_passing/qnx_dispatch/qnx_resource_path.cpp
+++ score/message_passing/qnx_dispatch/qnx_resource_path.cpp
@@ -14,7 +14,8 @@
 
 #include <score/assert.hpp>
 #include <score/utility.hpp>
-#include <cstddef>
+
+#include <iterator>
 
 namespace score
 {
@@ -23,12 +24,7 @@ namespace message_passing
 namespace detail
 {
 
-// constexpr std::size_t QnxResourcePath::kMaxIdentifierLen;
-
-// Suppress "AUTOSAR C++14 M5-0-15" rule finding: "Array indexing shall be the only form of pointer arithmetic".
-// Well-defined clear local use
 QnxResourcePath::QnxResourcePath(const std::string_view identifier) noexcept
-    // coverity[autosar_cpp14_m5_0_15_violation]
     : buffer_{GetQnxPrefix().cbegin(), GetQnxPrefix().cend()}
 {
     SCORE_LANGUAGE_FUTURECPP_PRECONDITION((identifier.size() > 0U) && (identifier.size() <= kMaxIdentifierLen));
@@ -36,8 +32,7 @@ QnxResourcePath::QnxResourcePath(const std::string_view identifier) noexcept
     auto identifier_begin = identifier.cbegin();
     if (*identifier_begin == '/')
     {
-        // coverity[autosar_cpp14_m5_0_15_violation]
-        ++identifier_begin;
+        identifier_begin = std::next(identifier_begin);
     }
     score::cpp::ignore = buffer_.insert(buffer_.cend(), identifier_begin, identifier.cend());
     buffer_.push_back('\0');  // AUTOSAR C++14 M5-0-11: Use character literal instead of plain integer
