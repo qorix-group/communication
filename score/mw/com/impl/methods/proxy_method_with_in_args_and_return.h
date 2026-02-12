762f947b2d5b848a4226c8c6c8b44b938443d374
George Dadunashvili <george.dadunashvili@bmw.de>
2026-02-12 17:09:38 +0100

    mw/com: Suppress clang-tidy cppcoreguidelines-pro-type-reinterpret-cast
    issue: SWP-239907
    GIT_ORIGIN_SPP_REV_ID: 5a93c06053affd9d9fd38be26a96f917666a13d8

--- score/mw/com/impl/methods/proxy_method_with_in_args_and_return.h
+++ score/mw/com/impl/methods/proxy_method_with_in_args_and_return.h
@@ -197,7 +197,18 @@ score::Result<MethodReturnTypePtr<ReturnType>> ProxyMethod<ReturnType(ArgTypes..
         return Unexpected(call_result.error());
     }
 
+    // reinterpret_cast is fine because we are casting back to the original type of this type-erased buffer.
+    // This object might be created by a different process but we require both processes to be compiled by the same
+    // compiler and compiler options, thus we are sure that the data can be interpreted correctly.
+    //  See AoU:
+    //  21206172
+    //  ScoreReq.AoU SameCompilerSettingsForProviderAndConsumerSide
+    //
+    // Additionally, we require the types to be trivially copyable.
+    // 5835098
+    // ScoreReq.AoU OnlyLoLaSupportedTypes
     return MethodReturnTypePtr<ReturnType>{
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast]) see above
         *(reinterpret_cast<ReturnType*>(allocated_return_type_storage.value().data())),
         is_return_type_ptr_active_[queue_position],
         queue_position};
