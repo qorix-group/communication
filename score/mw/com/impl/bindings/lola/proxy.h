67485a8034b8d0dfbae27e642ec256dd7d4c8745
Brendan Emery <brendan.be.emery@bmw.de>
2026-08-07 14:38:21 +0200

    lola: Remove unused function

--- score/mw/com/impl/bindings/lola/proxy.h
+++ score/mw/com/impl/bindings/lola/proxy.h
@@ -143,14 +143,6 @@ class Proxy : public ProxyBinding
     /// Terminates if the event control structure cannot be found.
     TransactionLogSet& GetTransactionLogSet(const ElementFqId element_fq_id);
 
-    /// Retrieves a reference to the event data storage area for a given ElementFqId.
-    ///
-    /// \param element_fq_id The Event ID.
-    /// \return A reference to the EventDataStorage.
-    template <typename EventSampleType>
-    auto GetEventDataStorage(const ElementFqId element_fq_id) const noexcept
-        -> const EventDataStorage<EventSampleType>&;
-
     /// Retrieves an event data meta info.
     ///
     /// The event meta info can be used to iterate over events in the event data storage when the type is not known e.g.
@@ -287,36 +279,6 @@ class Proxy : public ProxyBinding
     bool prepare_deinitialize_called_;
     bool finalize_deinitialize_called_;
 };
-
-template <typename EventSampleType>
-// Suppress "AUTOSAR C++14 A15-5-3" rule findings. This rule states: "The std::terminate() function shall not be called
-// implicitly". This is a false positive, std::less which is used by std::map::find could throw an exception if the
-// key value is not comparable and in our case the key is comparable. so no way for 'event_controls_.find()' to throw
-// an exception.
-// coverity[autosar_cpp14_a15_5_3_violation : FALSE]
-auto Proxy::GetEventDataStorage(const ElementFqId element_fq_id) const noexcept
-    -> const EventDataStorage<EventSampleType>&
-{
-    SCORE_LANGUAGE_FUTURECPP_PRECONDITION_PRD_MESSAGE(
-        data_ != nullptr, "Proxy::GetEventDataStorage: Managed memory data pointer is Null");
-    auto& service_data_storage = detail_proxy::GetServiceDataStorage(*data_);
-    const auto event_entry = service_data_storage.events_.find(element_fq_id);
-    if (event_entry == service_data_storage.events_.end())
-    {
-        score::mw::log::LogFatal("lola") << __func__ << __LINE__
-                                         << "Unable to find data storage for given event instance. Terminating.";
-        std::terminate();
-    }
-    // Suppress "AUTOSAR C++14 A5-3-2" rule finding. This rule declares: "Null pointers shall not be dereferenced.".
-    // The "event_entry" variable is an iterator of interprocess map returned by the "find" method.
-    // A check is made that the iterator is not equal to map.end(). Therefore, the call to "event_entry->"
-    // does not return nullptr.
-    // coverity[autosar_cpp14_a5_3_2_violation]
-    const auto* event_data_storage_ptr = event_entry->second.template get<EventDataStorage<EventSampleType>>();
-    SCORE_LANGUAGE_FUTURECPP_ASSERT_PRD_MESSAGE(event_data_storage_ptr != nullptr,
-                                                "Could not get EventDataStorage from OffsetPtr");
-    return *event_data_storage_ptr;
-}
 
 }  // namespace score::mw::com::impl::lola
 
